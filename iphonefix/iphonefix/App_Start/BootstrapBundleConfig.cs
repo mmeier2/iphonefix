7fe39c17808b383dcfd4a53c33f95c99cca97e01
Maddison Meier <maddisonm@axosoft.com>
2013-08-06 11:32:14 -0700

    initial imports of css files

--- iphonefix/iphonefix/App_Start/BootstrapBundleConfig.cs
+++ iphonefix/iphonefix/App_Start/BootstrapBundleConfig.cs
@@ -3,18 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
+
+[assembly: WebActivatorEx.PostApplicationStartMethod(typeof(iphonefix.App_Start.BootstrapBundleConfig), "RegisterBundles")]
 
 namespace iphonefix.App_Start
 {
-    public class BootstrapBundleConfig : Controller
+    public class BootstrapBundleConfig
     {
-        //
-        // GET: /BootstrapBundleConfig/
-
-        public ActionResult Index()
+        public static void RegisterBundles()
         {
-            return View();
+            // Add @Styles.Render("~/Content/bootstrap") in the <head/> of your _Layout.cshtml view
+            // Add @Scripts.Render("~/bundles/bootstrap") after jQuery in your _Layout.cshtml view
+            // When <compilation debug="true" />, MVC4 will render the full readable version. When set to <compilation debug="false" />, the minified version will be rendered automatically
+            BundleTable.Bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap*"));
+            BundleTable.Bundles.Add(new StyleBundle("~/Content/bootstrap").Include("~/Content/bootstrap.css", "~/Content/bootstrap-responsive.css"));
         }
-
     }
 }
---
+++ iphonefix/iphonefix/Infrastructure/Utils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace iphonefix.Infrastructure
+{
+    public class Utils
+    {
+        public static string GetSiteRoot()
+        {
+            return HttpContext.Current.Request.Url.Scheme
+                + "://"
+                + HttpContext.Current.Request.Url.Authority
+                + HttpContext.Current.Request.ApplicationPath;
+        }
+    }
+}

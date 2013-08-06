dd2a7bdc7d32224b1c568b166401b15b28f6d7b0
Maddison Meier <maddisonm@axosoft.com>
2013-08-06 09:36:07 -0700

    added bootstrap and toastr scripts

---
+++ iphonefix/iphonefix/App_Start/BootstrapBundleConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace iphonefix.App_Start
+{
+    public class BootstrapBundleConfig : Controller
+    {
+        //
+        // GET: /BootstrapBundleConfig/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
